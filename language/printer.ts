6a9e3b87bbed68453f7d6f7a90802045d0bcfb8f
Drew Thomas <cdthomas.92@gmail.com>
2020-03-07 14:10:03 +1100

    Add printer

---
+++ language/printer.ts
@@ -0,0 +1,26 @@
+import { Node, ASTKind } from "./ast";
+
+function print(node: Node): string {
+  switch (node.kind) {
+    case ASTKind.Bool:
+      return String(node.value);
+    case ASTKind.ExpressionStatement:
+      return print(node.expression) + ";";
+    case ASTKind.Identifier:
+      return node.value;
+    case ASTKind.InfixExpression:
+      return `(${print(node.left)} ${node.operator} ${print(node.right)})`;
+    case ASTKind.Integer:
+      return String(node.value);
+    case ASTKind.Let:
+      return `let ${print(node.name)};`;
+    case ASTKind.PrefixExpression:
+      return `${node.operator}${print(node.right)}`;
+    case ASTKind.Program:
+      return node.statements.map(print).join("\n");
+    case ASTKind.Return:
+      return "return;";
+  }
+}
+
+export { print };
