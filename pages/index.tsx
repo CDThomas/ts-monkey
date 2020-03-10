a91fd35f10353de73d60d6f43f98a9c55f343436
Drew Thomas <cdthomas.92@gmail.com>
2020-03-10 19:39:33 +1100

    Output AST instead of tokens in UI (#19)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import React from "react";
+import React, { useState, useCallback } from "react";
+import debounce from "debounce";
 
 const Editor = dynamic(import("../components/Editor"), { ssr: false });
+import { Program } from "../language/ast";
 import Lexer from "../language/lexer";
-import { Token, TokenKind } from "../language/token";
+import Parser from "../language/parser";
 import styles from "./index.module.css";
 
 const defaultInput = `let five = 5;
@@ -17,16 +19,30 @@ let add = fn(x, y) {
 let result = add(five, ten);
 `;
 
-function readTokens(lexer: Lexer, tokens: Token[] = []): Token[] {
-  const token = lexer.nextToken();
-
-  if (token.kind === TokenKind.EOF) return tokens;
-  return readTokens(lexer, [...tokens, token]);
-}
+const parse = (input: string): Program => {
+  const lexer = new Lexer(input);
+  const parser = new Parser(lexer);
+  return parser.parseProgram();
+};
 
 function Home(): React.ReactElement {
-  const [input, setInput] = React.useState(defaultInput);
-  const output = readTokens(new Lexer(input));
+  const [input, setInput] = useState(defaultInput);
+  const [output, setOutput] = useState<Program>(parse(defaultInput));
+  const doParse = useCallback(
+    debounce((value: string) => {
+      try {
+        const output = parse(value);
+        setOutput(output);
+      } catch (error) {
+        console.error(error);
+      }
+    }, 200),
+    []
+  );
+  const handleChange = useCallback((value: string): void => {
+    setInput(value);
+    doParse(value);
+  }, []);
 
   return (
     <div>
@@ -36,7 +52,7 @@ function Home(): React.ReactElement {
       </Head>
 
       <main className={styles.container}>
-        <Editor value={input} onChange={(input): void => setInput(input)} />
+        <Editor value={input} onChange={handleChange} />
         <Editor readOnly value={JSON.stringify(output, null, 2)} />
       </main>
     </div>
