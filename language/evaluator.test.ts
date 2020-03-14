c01a2ef021cb49ff5fb521f8e2746d06ff07a5fb
Drew Thomas <cdthomas.92@gmail.com>
2020-03-14 15:51:52 +1100

    Evaluate prefix expressions (#21)
    * Evaluate bang operator expressions
    
    * Evaluate minus operator expressions

--- language/evaluator.test.ts
+++ language/evaluator.test.ts
@@ -16,11 +16,12 @@ describe("evaluating", () => {
     const cases = [
       { input: "0", expected: 0 },
       { input: "5", expected: 5 },
-      { input: "10", expected: 10 }
+      { input: "-5", expected: -5 },
+      { input: "-10", expected: -10 }
     ];
 
     cases.forEach(({ input, expected }) => {
-      test(`${input} evaluates to the correct value`, () => {
+      test(`${input} evaluates to ${expected}`, () => {
         const actual = doEval(input);
         expect(actual).toBeInstanceOf(Integer);
         expect((actual as Integer).value).toBe(expected);
@@ -29,16 +30,64 @@ describe("evaluating", () => {
   });
 
   describe("boolean expressions", () => {
-    test("true evaluates to the correct value", () => {
+    test("true evaluates to true", () => {
       const result = doEval("true");
       expect(result).toBeInstanceOf(Bool);
       expect((result as Bool).value).toBe(true);
     });
 
-    test("false evaluates to the correct value", () => {
+    test("false evaluates to false", () => {
       const result = doEval("false");
       expect(result).toBeInstanceOf(Bool);
       expect((result as Bool).value).toBe(false);
+    });
+
+    test("throw an error when used with the minus prefix operator", () => {
+      expect(() => doEval("-true")).toThrow(
+        "evaluation error: minus prefix operator not implemented for true"
+      );
+    });
+  });
+
+  describe("the bang prefix operator", () => {
+    const cases = [
+      {
+        input: "!true",
+        expected: false,
+        description: "negating true evaluates to false"
+      },
+      {
+        input: "!false",
+        expected: true,
+        description: "negating false evalutes to true"
+      },
+      {
+        input: "!5",
+        expected: false,
+        description: "negating an int coerces and evaluates to false"
+      },
+      {
+        input: "!!true",
+        expected: true,
+        description: "double negating true evaluates to true"
+      },
+      {
+        input: "!!false",
+        expected: false,
+        description: "double negating false evaluates to false"
+      },
+      {
+        input: "!!5",
+        expected: true,
+        description: "double negating an int coerces and evaluates to true"
+      }
+    ];
+
+    cases.forEach(({ input, expected, description }) => {
+      test(description, () => {
+        const result = doEval(input);
+        expect((result as Bool).value).toBe(expected);
+      });
     });
   });
 });
--- language/evaluator.ts
+++ language/evaluator.ts
@@ -13,12 +13,53 @@ export function evaluate(node: Node): Obj {
       return evaluate(node.expression);
     case ASTKind.Integer:
       return new Integer(node.value);
+    case ASTKind.PrefixExpression: {
+      const right = evaluate(node.right);
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      return evalPrefixExpression(node.operator, right);
+    }
     case ASTKind.Program:
       // eslint-disable-next-line @typescript-eslint/no-use-before-define
       return evalStatements(node.statements);
   }
 
   throw new Error(`eval not implemented for ${node.kind}`);
+}
+
+function evalPrefixExpression(operator: string, right: Obj): Obj {
+  switch (operator) {
+    case "!":
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      return evalBangOperatorExpression(right);
+    case "-":
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      return evalMinusPrefixOperatorExpression(right);
+    default:
+      return NULL;
+  }
+}
+
+function evalBangOperatorExpression(right: Obj): Obj {
+  switch (right) {
+    case TRUE:
+      return FALSE;
+    case FALSE:
+      return TRUE;
+    case NULL:
+      return TRUE;
+    default:
+      return FALSE;
+  }
+}
+
+function evalMinusPrefixOperatorExpression(right: Obj): Obj {
+  if (right instanceof Integer) {
+    return new Integer(-right.value);
+  }
+
+  throw new Error(
+    `evaluation error: minus prefix operator not implemented for ${right.inspect()}`
+  );
 }
 
 function evalStatements(statements: Statement[]): Obj {
